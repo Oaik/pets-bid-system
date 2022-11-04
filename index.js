72d88627ead1e234703a3c21642bb301a6d766b8
Omar Morsy <omarashrf15@gmail.com>
2022-11-04 17:14:43 +0200

    Removed swagger setup in index.js

--- index.js
+++ index.js
@@ -2,14 +2,9 @@ const dotenv = require('dotenv');
 dotenv.config();
 
 const express = require('express');
-const swaggerUi = require('swagger-ui-express');
-const swaggerDocument = require('./api/swagger.json');
 
 const app = new express();
 app.use(express.json());
-
-/* set up swagger in the root */
-app.use('/', swaggerUi.serve, swaggerUi.setup(swaggerDocument))
 
 app.listen(process.env.PORT, () => {
     console.log(`Server is working on port ${process.env.PORT}`);
