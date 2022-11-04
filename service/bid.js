e47db0341c97031aa4c184cfe4646eab49d359fe
Omar Morsy <omarashrf15@gmail.com>
2022-11-04 19:01:28 +0200

    implement placeBid in bid service

--- service/bid.js
+++ service/bid.js
@@ -2,6 +2,7 @@ const bidDal = require('../dal/bid');
 
 module.exports.getBids = async function(bid, rpi, rps) {
     console.log("-----Service:getBids-----")
+
     const petId = bid.petId;
     const query = {
         petId: petId
@@ -9,4 +10,17 @@ module.exports.getBids = async function(bid, rpi, rps) {
 
     const allBids = await bidDal.getBidsByPetId(query, rpi, rps);
     return allBids;
+}
+
+module.exports.placeBid = async function(bid) {
+    console.log("-----Service:placeBid-----")
+
+    const query = {
+        petId: bid.petId,
+        ownerId: bid.ownerId,
+        userId: bid.userId,
+        price: bid.price
+    };
+
+    await bidDal.placeBid(query);
 }
