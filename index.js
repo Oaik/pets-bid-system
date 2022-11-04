402af46249aa1c21f25f1a4132ba7cc226c2bf6f
Omar Morsy <omarashrf15@gmail.com>
2022-11-04 19:36:09 +0200

    Add getBids and palceBid route

--- index.js
+++ index.js
@@ -4,10 +4,14 @@ dotenv.config();
 const express = require('express');
 const dbManager = require('./db.js');
 
+const bidRouter = require('./route/bid.js')
+
 const app = new express();
 app.use(express.json());
 
 dbManager.dbConnect();
+
+app.use('/bid', bidRouter)
 
 app.listen(process.env.PORT, () => {
     console.log(`Server is working on port ${process.env.PORT}`);
