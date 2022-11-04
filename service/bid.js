85df63d240ab565fcffa86b012fb0618458f5092
Omar Morsy <omarashrf15@gmail.com>
2022-11-04 18:38:23 +0200

    add query object in getBids in bid service

--- service/bid.js
+++ service/bid.js
@@ -3,7 +3,10 @@ const bidDal = require('../dal/bid');
 module.exports.getBids = async function(bid, rpi, rps) {
     console.log("-----Service:getBids-----")
     const petId = bid.petId;
+    const query = {
+        petId: petId
+    }
 
-    const allBids = await bidDal.getBidsByPetId(petId, rpi, rps);
+    const allBids = await bidDal.getBidsByPetId(query, rpi, rps);
     return allBids;
 }
