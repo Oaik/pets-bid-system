fe7e0b07f55b6809a9ede62f41a03da6391643ff
Omar Morsy <omarashrf15@gmail.com>
2022-11-04 20:11:53 +0200

    Specify returned fields in bid

--- dal/bid.js
+++ dal/bid.js
@@ -4,7 +4,26 @@ module.exports.getBidsByPetId = async function(query, rpi, rps) {
     console.log("-----dal:getBidsByPetId-----")
     
     try {
-        const bids = await bidModel.find(query).skip(rpi * rps).limit(rps);
+        const bids = await bidModel.aggregate([
+            {
+              $match: query
+            },
+            {
+                $skip: rpi * rps
+            },
+            {
+              $limit: rps
+            },
+            {
+                $project: {
+                    _id: 0,
+                    petId: 1,
+                    ownerId: 1,
+                    userId: 1,
+                    price: 1
+                }
+            }
+          ]);
         return bids;
     } catch(error) {
         console.error("Error in getBidsByPetId Case 0", '\n', error);
